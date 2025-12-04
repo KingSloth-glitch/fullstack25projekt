3a2bce978b9ead25b08f09da41ac1ced662b74ac
Pontus <historicalkingsloth@gmail.com>
2025-12-04 09:24:54 +0100

    javascript

---
+++ javascript/logik.js
@@ -0,0 +1,5 @@
+function calculateSum(a, b) {
+var sum = a + b;
+console.log("Summan är: " + sum);
+}
+calculateSum(10, 20);
---
+++ javascript/palindromtemp.js
@@ -0,0 +1,11 @@
+function ispalindrome(text) {
+    let cleaned = text.toLowerCase().replace(/\s+/g, "");
+
+    let reversed = cleaned.split("").reverse().join("");
+
+    if (cleaned === reversed) {
+        return "true";
+    } else {
+        return "false";
+    }
+    }
